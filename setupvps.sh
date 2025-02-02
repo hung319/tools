38a326142737e604b36393ba06b6e29a66f8632d
Hoàng Phi Hùng <83570379+hung319@users.noreply.github.com>
2025-02-02 18:42:21 +0700

    Create setupvps.sh

---
+++ setupvps.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# cập nhập OS
+sudo apt update && sudo apt upgrade -y
+
+# set locale
+locale-gen en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
+# Tắt Firewall
+sudo apt remove iptables-persistent -y
+sudo ufw disable
+sudo iptables -F
+
+# Chỉnh về múi giờ Việt Nam
+timedatectl set-timezone Asia/Ho_Chi_Minh
+
+# Tạo swap 2GB RAM
+sudo fallocate -l 2G /swapfile && sudo chmod 600 /swapfile && sudo mkswap /swapfile && sudo swapon /swapfile && echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+cat <<EOF > /etc/sysctl.d/99-xs-swappiness.conf
+vm.swappiness=10
+EOF
+
+# Enable TCP BBR congestion control
+cat <<EOF > /etc/sysctl.conf
+net.core.default_qdisc=fq
+net.ipv4.tcp_congestion_control=bbr
+EOF
+
+# Cài đặt docker và docker-compose
+curl -fsSL https://get.docker.com -o get-docker.sh 
+sh get-docker.sh
