f08d41783805e3e2451fe87ecab503189b4ed131
Hoàng Phi Hùng <83570379+hung319@users.noreply.github.com>
2025-09-12 05:25:38 +0700

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -4,25 +4,24 @@ set -e
 # Thư mục cài đặt
 PREFIX="$HOME/nginx"
 
-# Tạo thư mục tạm
+# Phiên bản Nginx
+NGINX_VERSION=1.26.2
+
+# Thư mục tạm
 TMPDIR=$(mktemp -d)
 cd "$TMPDIR"
 
-# Phiên bản nginx muốn cài
-NGINX_VERSION=1.26.2
-
 echo "Tải Nginx $NGINX_VERSION..."
 curl -L "http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz" -o nginx.tar.gz
-
 tar xzf nginx.tar.gz
 cd nginx-${NGINX_VERSION}
 
-echo "Biên dịch Nginx..."
+echo "Biên dịch Nginx với gzip..."
 ./configure \
   --prefix="$PREFIX" \
   --with-http_ssl_module \
-  --without-http_rewrite_module \
-  --without-http_gzip_module
+  --with-http_gzip_static_module \
+  --with-http_v2_module
 
 make -j"$(nproc)"
 make install
@@ -30,6 +29,8 @@ make install
 echo "Đã cài Nginx vào $PREFIX"
 echo "Chạy thử: $PREFIX/sbin/nginx -v"
 
-# Gợi ý PATH
-echo 'export PATH="$HOME/nginx/sbin:$PATH"' >> "$HOME/.bashrc"
+# Thêm vào PATH
+if ! grep -q 'nginx/sbin' "$HOME/.bashrc"; then
+  echo 'export PATH="$HOME/nginx/sbin:$PATH"' >> "$HOME/.bashrc"
+fi
 echo "Đã thêm Nginx vào PATH. Đăng nhập lại shell để dùng lệnh nginx."
