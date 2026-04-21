b5f1ad1756066f5898cb3531af87aafc10dc2e80
Hoàng Phi Hùng <83570379+hung319@users.noreply.github.com>
2026-04-21 11:21:10 +0700

    Update dockcheck.sh

--- dockcheck.sh
+++ dockcheck.sh
@@ -1,1 +1,7 @@
 curl -L https://raw.githubusercontent.com/mag37/dockcheck/main/dockcheck.sh -o /usr/local/bin/dockcheck.sh && chmod +x /usr/local/bin/dockcheck.sh && /usr/local/bin/dockcheck.sh -v && (crontab -l 2>/dev/null | grep -v "/usr/local/bin/dockcheck.sh"; echo "0 4 * * * /usr/local/bin/dockcheck.sh -ai -p > /dev/null 2>&1") | crontab - && echo "Xong! Dockcheck đã cài và đặt lịch 4h sáng hàng ngày."
+
+
+sudo rm /usr/local/bin/dockcheck.sh && \
+sudo rm /usr/local/bin/regctl && \
+(crontab -l 2>/dev/null | grep -v "/usr/local/bin/dockcheck.sh") | crontab - && \
+echo "Đã gỡ bỏ Dockcheck và lịch chạy tự động."
